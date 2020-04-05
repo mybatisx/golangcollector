373cc1a58e58af2dc6224a3a1d5a647e1c5895f8
ming <ming@mings-MacBook-Pro.local>
2020-04-05 11:16:45 +0800

    6

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
-import "cookbook/collector"
+import (
+	"cookbook/global"
+)
 
 func main() {
 
 
 
-	//ch := make(chan struct{})
-	//global.MyGlobal().Server.Run()
-	//ch <- struct{}{}
+	ch := make(chan struct{})
+	global.MyGlobal().Server.Run()
+	ch <- struct{}{}
 	//imgUrl:=collector.UploadImg("http://img2.bdstatic.com/img/image/166314e251f95cad1c8f496ad547d3e6709c93d5197.jpg")
 	//log.Printf(imgUrl)
 
-	box:= collector.Box{}
-	box.Run()
+	//box:= collector.Box{}
+	//box.Run()
 }
