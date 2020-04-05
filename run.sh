ac6f7d699d712d738cb936825748403fb2eefd00
ming <ming@mings-MacBook-Pro.local>
2020-04-05 09:49:32 +0800

    K

--- run.sh
+++ run.sh
@@ -3,5 +3,14 @@
 git pull origin master
 
 docker build -t lanrenshipu:v1 .
+docker rm -f lanrenshipu
+docker run -d -p 10002:8080 --name lanrenshipu -v /home/assets:/home/assets lanrenshipu:v1
 
-docker run -d -p 10002:8080 -v /home/assets:/home/assets lanrenshipu:v1
+docker rm -f nginx
+docker run \
+  --name nginx \
+  -d -p 81:80 \
+  -v /home/github.com/nginx/html:/usr/share/nginx/html \
+  -v /home/github.com/nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
+  -v /home/github.com/nginx/conf.d:/etc/nginx/conf.d \
+  nginx
